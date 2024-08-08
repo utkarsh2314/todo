9009561e81b2505d41b0e8ca9ab21db4c64f81e6
utkarsh2314 <utkarshukla2003@gmail.com>
2024-08-08 21:46:04 +0530

    file stucture made

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
 import React from "react";
 import "./App.css";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/todoList";
+
+const [todo, setTodo] = useState([]);
 
 function App() {
-  return <>Hello React</>;
+  return (
+    <>
+      <Navbar />
+      <TodoForm />
+      <TodoList />
+      <footer>&copy; peace out ;-O</footer>
+    </>
+  );
 }
 
 export default App;
